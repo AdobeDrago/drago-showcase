2abf07757d1941991898220fe951d708fe48fe88
timwlee <timothyl@adobe.com>
2026-06-09 12:18:51 -0700

    Notes projects-listing block and starter notes

---
+++ blocks/projects-listing/projects-listing.js
@@ -0,0 +1,9 @@
+export default async function decorate(block) {
+ console.log("block: ", block);
+
+ // 1. grab the href value of <a> link
+ // 2. Fetch the JSON data from the link
+ // 3. Using the data object, create the necessary HTML elements and populate them with the data
+
+ 
+}
